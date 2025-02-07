abe0d4629c02f703219be2180ee052a20dde7ff2
Jenna-Kei <157016286+Jenna-Kei@users.noreply.github.com>
2025-02-08 00:09:54 +0800

    Update Preprocessing.py

--- ai/Preprocessing.py
+++ ai/Preprocessing.py
@@ -2,11 +2,12 @@ import pandas as pd
 
 # Noise removal/ Outlier removal
 
+
 # Image Filtering/ Cropping
 
 # Image Resizing (allow Multi-Resolution Training)
 from PIL import Image 
-size = (40, 40)
+size = (28, 28) # for the trial workshop
 img = Image.open(r"test.png")
 r_img = img.resize(size)
 r_img.show()
@@ -15,8 +16,13 @@ r_img.show()
 from PIL import Image, ImageOps
 im2 = ImageOps.grayscale(im1) 
 im2.show()
+# OR: 
+import cv2
+# Use the cvtColor() function to grayscale the image
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Splitting the Dataset
+
 
 # Label Encoding  # https://www.geeksforgeeks.org/ml-label-encoding-of-datasets-in-python/
 # Import label encoder 
@@ -29,6 +35,19 @@ df['species']= label_encoder.fit_transform(df['species'])
 df['species'].unique() 
 
 # Data Shuffling
+# train/test
+from sklearn.model_selection import train_test_split
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.2, random_state=42)
+# train/val/test
+from sklearn.model_selection import train_test_split
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.2, random_state=42)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+# k-fold CV
+from sklearn.model_selection import KFold
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
+for train_index, val_index in kf.split(X):
+    X_train, X_val = X[train_index], X[val_index]
+    y_train, y_val = y[train_index], y[val_index]
 
 # Normalization
 
