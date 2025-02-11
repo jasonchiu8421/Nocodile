3e5dd41804333bd992b040be9fc8ccb355529349
jasonchiu8421 <114208870+jasonchiu8421@users.noreply.github.com>
2025-02-11 17:47:50 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 from ai/ModelTraining import pretraining, FlexibleCNN
 from ai/Dataset import DatasetCreator, DatasetLoader
+from pandas import pd
 
 # Load image data from directory (suppose we have a folder called digits in current directory)
 base_folder = 'digits'
@@ -8,26 +9,53 @@ dataset_creator.load_data()
 filename = "digits_dataset"
 dataset_creator.save_dataset(filename)
 
-# Load saved dataset, print shapes and first image per label
+# Load saved dataset, print shapes and first image per class
 dataset_loader = DatasetLoader()
 dataset = dataset_loader.load_saved_dataset('digits_dataset')
 dataset_loader.print_shapes()
 dataset_loader.print_first_image_per_label()
 
-# Save dataset after some image processing
+# Save dataset
 dataset_loader = DatasetLoader(images, labels)
 dataset_loader.save_dataset('digits_dataset')
 
-# Train model1 from kaggle (assuming train.csv is in 'ai' directory)
-x = pretraining()
-x.load_csv("train.csv")
-X_train, y_train = x.preprocessing()
-model1 = FlexibleCNN(X_train, y_train)
-model1.pretraining()
-model1.model1()
-model1.check_performance()
+# Visualize the shapes of images and labels
+dataset_loader = DatasetLoader(images, labels)
+dataset_loader.print_shapes()
 
-# use the model
-x = pretraining()
-X_test = x.load_csv("test.csv")
-model1.run_model(X_test)
+# Visualize the first image per class
+dataset_loader = DatasetLoader(images, labels)
+dataset_loader.print_first_image_per_label()
+
+# Train model (assuming train.csv is in 'ai' directory) using train/test approach
+x = LoadProcessedData()
+x.load_csv("train.csv")
+X, y = x.preprocessing()
+cnn = FlexibleCNN(X, y)
+model1 = cnn.train_test_approach('model1') # argument is the name of the model
+  # This will split data, train model, and show performance of the model
+
+# Train model (assuming train.csv is in 'ai' directory) using train/test/val approach
+x = LoadProcessedData()
+x.load_csv("train.csv")
+X, y = x.preprocessing()
+cnn = FlexibleCNN(X, y)
+model1= cnn.train_test__val_approach('model1') # argument is the name of the model
+  # This will split data, train model, and show performance of the model
+
+# Train model (assuming train.csv is in 'ai' directory) using k fold validation method
+x = LoadProcessedData()
+x.load_csv("train.csv")
+X, y = x.preprocessing()
+cnn = FlexibleCNN(X, y)
+model1 = cnn.kFold_validation_approach('model1') # argument is the name of the model
+  # This will split data, train model, and show performance of the model
+
+# Find prediction using the model (immediately after training) assuming 'test.csv’ is in current directory
+X_test = pd.read_csv('test.csv')
+prediction = cnn.run_model(X_test)
+
+# Find prediction using the model (at any time)
+X_test = pd.read_csv('test.csv')
+cnn = FlexibleCNN()
+prediction = cnn.run_model(X_test, model1) # model 1 is the model trained
