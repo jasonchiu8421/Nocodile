c7f8a43c1d43dd2f23549a5a4b7d795486cb31c2
jasonchiu8421 <114208870+jasonchiu8421@users.noreply.github.com>
2025-03-21 10:12:10 +0800

    Update server2.py

--- ai/server2.py
+++ ai/server2.py
@@ -67,23 +67,6 @@ class PredictionRequest(BaseModel):
     image: str
     model_path: str
 
-# no use?
-class CustomDataset(Dataset):
-    def __init__(self, images, labels, transform=None):
-        self.images = images
-        self.labels = labels
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, idx):
-        image = self.images[idx]
-        label = self.labels[idx]
-        if self.transform:
-            image = self.transform(image)
-        return image, label
-    
 class DatasetCreator:
     def __init__(self, base_folder):
         self.base_folder = base_folder
