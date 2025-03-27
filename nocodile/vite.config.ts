bddc4c71bcabd68f71bb069f20b6d02031bdbc92
shedaniel <daniel@shedaniel.me>
2025-03-27 15:39:59 +0800

    feat: Use postcss to support more browser

--- nocodile/vite.config.ts
+++ nocodile/vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 import { defineConfig } from "vite"
+import postcssPresetEnv from "postcss-preset-env"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +12,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  css: {
+    postcss: {
+      plugins:[
+        postcssPresetEnv({
+          browsers: ["supports css-matches-pseudo and > 0.1% and not op_mini all and not dead"]
+        })
+      ]
+    }
+  }
 })
