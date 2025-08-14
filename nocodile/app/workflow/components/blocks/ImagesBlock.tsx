0993258e85a71e23db9d3f1a277eba97556efb6a
kristen wong <kristenccwong@gmail.com>
2025-08-14 14:09:04 +0800

    state for imagesblock

--- nocodile/app/workflow/components/blocks/ImagesBlock.tsx
+++ nocodile/app/workflow/components/blocks/ImagesBlock.tsx
@@ -17,8 +17,25 @@ type ImagesBlockProps = {
 };
 
 export const ImagesBlock = ({ block }: ImagesBlockProps) => {
+  const [files, setFiles] = React.useState<File[]>([]);
+  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const selectedFiles = event.target.files;
+    if (selectedFiles) {
+      const fileArray = Array.from(selectedFiles);
+      setFiles(fileArray);
+    }
+  }
+
   return (
     <DraggableBlock id={block.id} x={block.x} y={block.y}>
+      <h1>Images block</h1>
+      <small>Type: {block.type}</small>
+      {files.length > 0 ? (
+        <div className="bg-gray-500 p-2 rounded-lg">Add images here...</div>
+      ) : (
+        <div>files.length+" images"</div>
+      )}
+
       <div>
         <input type="file" multiple accept="image/*" />
       </div>
